22613f035da965fbe331ecfd3053470dbf8133a7
unknown <reactjs35@gmail.com>
2025-11-26 12:53:23 +0700

    fix

--- src/Home/HomePage.jsx
+++ src/Home/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import Slider from '../components/Slider'
+import Container from '../components/Container'
 
 const HomePage = () => {
   return (
     <div>
         <Header/>
         <Slider/>
+        <Container/>
     </div>
   )
 }
